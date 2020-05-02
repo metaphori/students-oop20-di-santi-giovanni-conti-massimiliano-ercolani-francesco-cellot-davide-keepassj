fe5fc98b2f4ffe2814aaff990041fd80de4a29e1
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-05-02 16:31:16 +0200

    Change TestView.java into GUITest.java

---
+++ src/main/java/view/GUITest.java
@@ -0,0 +1,19 @@
+package view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+public class GUITest extends Application {
+    public static void main(final String[] args) {
+        launch(args);
+    }
+    
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello World");
+        Scene scene = new Scene(label);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
