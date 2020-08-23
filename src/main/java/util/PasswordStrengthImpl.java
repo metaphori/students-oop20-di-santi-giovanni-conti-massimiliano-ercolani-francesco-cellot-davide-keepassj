936733452cdeda40b6cc642b84e3be98c22d67e8
Davide99 <davidecellot@gmail.com>
2020-08-23 15:05:35 +0200

    Create class PasswordStrengthImpl

---
+++ src/main/java/util/PasswordStrengthImpl.java
@@ -0,0 +1,11 @@
+package util;
+
+public class PasswordStrengthImpl implements PasswordStrength{
+
+	@Override
+	public int getStrength(String password) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
