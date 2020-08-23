dee761432e15cf33bb6e2f92a421eda78f69de30
Davide99 <davidecellot@gmail.com>
2020-08-23 14:57:24 +0200

    Add method to create random password

--- src/main/java/util/GeneratePasswordRandomImpl.java
+++ src/main/java/util/GeneratePasswordRandomImpl.java
@@ -1,5 +1,21 @@
 package util;
 
-public class GeneratePasswordRandomImpl {
+public class GeneratePasswordRandomImpl extends PasswordValidatorImpl implements GeneratePasswordRandom{
 
+	static Random rnd_method;
+
+	
+	public String generatePassword() {
+		String psw;
+		
+		/*Genero password diverse casuali finchè non ne trovo una valida*/
+		while(true) {
+			psw = randomPassword(8);
+			/*Uso la funzione isValid() della classe PasswordValidatorImpl*/
+			if(isValid(psw))
+				return psw;
+		}
+	}
+	
+	
 }
