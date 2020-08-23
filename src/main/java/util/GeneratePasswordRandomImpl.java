48a05d098e33d697d37b23b87edf807c2a98b268
Davide99 <davidecellot@gmail.com>
2020-08-23 14:59:03 +0200

    Add private method to create password

--- src/main/java/util/GeneratePasswordRandomImpl.java
+++ src/main/java/util/GeneratePasswordRandomImpl.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.Random;
+
 public class GeneratePasswordRandomImpl extends PasswordValidatorImpl implements GeneratePasswordRandom{
 
 	static Random rnd_method;
@@ -16,6 +18,26 @@ public class GeneratePasswordRandomImpl extends PasswordValidatorImpl implements
 				return psw;
 		}
 	}
+	
+	private static String randomPassword(int len) {
+		rnd_method = new Random();
+		char[] password = new char[len];
+		String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+		String Small_chars = "abcdefghijklmnopqrstuvwxyz"; 
+		String numbers = "0123456789"; 
+		String symbols = "!@#$%^&*_=+-/.?";
+		String values = Capital_chars + Small_chars + numbers + symbols;
+		
+		/*Inserisco carattere scelto casualmente nel vettore*/
+		for(int i=0; i<len; i++) {
+			password[i] = values.charAt(rnd_method.nextInt(values.length()));
+		}
+		
+		/*Converto il vettore di char in stringa*/
+		String a = new String(password);
+		
+		return a;
+	} 
 	
 	
 }
