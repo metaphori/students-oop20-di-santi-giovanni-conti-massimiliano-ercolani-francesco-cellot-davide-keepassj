9cae95c8abb3fc1d7f30376d4fe00b2f024db39d
CellotDavide <davidecellot@gmail.com>
2020-08-27 12:37:51 +0200

    Insert new method in the interface

--- src/main/java/util/PasswordStrength.java
+++ src/main/java/util/PasswordStrength.java
@@ -2,5 +2,10 @@ package util;
 
 public interface PasswordStrength {
 
+	/*Method that return the password strength, passed by param*/
 	int getStrength(String password);
+	
+	/*Method that return the number of characters*/
+	int getLength();
+	
 }
