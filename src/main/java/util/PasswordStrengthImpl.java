dea2f914e830bf669c565ce8ecde231ebe63faa4
CellotDavide <davidecellot@gmail.com>
2020-08-27 14:48:00 +0200

    Insert new private methods useful to calcolate password strength

--- src/main/java/util/PasswordStrengthImpl.java
+++ src/main/java/util/PasswordStrengthImpl.java
@@ -16,6 +16,25 @@ public class PasswordStrengthImpl implements PasswordStrength{
 		return this.password.lenght;
 	}
 
+	/*Return the number of Uppercase letters*/
+	private int getNUpper() {
+		int count=0;
+		
+		for(int i=0; i<this.password.length; i++) {
+			if(password[i] >= 'A' && password[i] <= 'Z')
+				count++;
+		}
+		return count;
+	}
 	
+	/*Return the number of lowercase letters*/
+	private int getNLower();
+	
+	/*Return the number of digit*/
+	private int getNNumbers();
+	
+	/*Return the number of special characters*/
+	private int getNSpecial();
+
 	
 }
