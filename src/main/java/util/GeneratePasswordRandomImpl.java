d29e007fd29c4c06ac946b1824a0c1947a860685
CellotDavide <davidecellot@gmail.com>
2020-08-28 14:36:03 +0200

    Fix all the checkstyle error in this class

--- src/main/java/util/GeneratePasswordRandomImpl.java
+++ src/main/java/util/GeneratePasswordRandomImpl.java
@@ -2,45 +2,44 @@ package util;
 
 import java.util.Random;
 
-public class GeneratePasswordRandomImpl extends PasswordValidatorImpl implements GeneratePasswordRandom{
+public class GeneratePasswordRandomImpl extends PasswordValidatorImpl implements GeneratePasswordRandom {
 
-	static Random rnd_method;
+    private static Random random;
 
-	public GeneratePasswordRandomImpl() {
-		
-		
-	}
-	
-	public String generatePassword() {
-		String psw;
-		
-		/*Generate different random password until find a valid one*/
-		while(true) {
-			psw = randomPassword(8);
-			/*Use isValid() function of the class: "PasswordValidatoImpl"*/
-			if(isValid(psw))
-				return psw;
-		}
-	}
-	
-	private static String randomPassword(int len) {
-		rnd_method = new Random();
-		char[] password = new char[len];
-		String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
-		String Small_chars = "abcdefghijklmnopqrstuvwxyz"; 
-		String numbers = "0123456789"; 
-		String symbols = "!@#$%^&*_=+-/.?";
-		String values = Capital_chars + Small_chars + numbers + symbols;
-		
-		/*Insert randomly chosen character in the vector*/
-		for(int i=0; i<len; i++) {
-			password[i] = values.charAt(rnd_method.nextInt(values.length()));
-		}
-		
-		/*Convert the char vector to string*/
-		String a = new String(password);
-		
-		return a;
-	} 
-	
+    public GeneratePasswordRandomImpl() {
+    }
+
+     public String generatePassword() {
+        String psw;
+
+        /*Generate different random password until find a valid one*/
+        while (true) {
+            psw = randomPassword(8);
+            /*Use isValid() function of the class: "PasswordValidatoImpl"*/
+            if (isValid(psw)) {
+               return psw;
+            }
+        }
+    }
+
+    private static String randomPassword(final int len) {
+        random = new Random();
+        char[] password = new char[len];
+        String upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+        String lowerLetters = "abcdefghijklmnopqrstuvwxyz"; 
+        String numbers = "0123456789"; 
+        String symbols = "!@#$%^&*_=+-/.?";
+        String values = upperLetters + lowerLetters + numbers + symbols;
+
+        /*Insert randomly chosen character in the vector*/
+        for (int i = 0; i < len; i++) {
+            password[i] = values.charAt(random.nextInt(values.length()));
+        }
+
+        /*Convert the char vector to string*/
+        String a = new String(password);
+
+        return a;
+    } 
+
 }
