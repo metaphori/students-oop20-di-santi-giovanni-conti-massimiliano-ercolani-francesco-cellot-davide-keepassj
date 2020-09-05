9a8439d14581b8e04cea8f88903016fbc809cea9
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2020-09-05 10:21:32 +0200

    Add getEntry and delEntry to Datab

--- src/main/java/model/db/Datab.java
+++ src/main/java/model/db/Datab.java
@@ -10,11 +10,33 @@ public class Datab {
         this.mydb = new ArrayList<>();
     }
 
+    /**
+     * Receive a new entry to insert.
+     * @param entry
+     * @return the state of execution if true it's done
+     */
     public final boolean addEntry(final ElementDb entry) {
         if (nameAlreadyExist(entry.getNameAccount())) {
             return false;
         }
         this.mydb.add(entry);
+        return true;
+    }
+
+    /**
+     * Receive a new entry to insert.
+     * @param nameToDelete
+     * @return the state of execution if true it's done
+     */
+    public final boolean delEntry(final String nameToDelete) {
+        ElementDb temp;
+        if (getEntry(nameToDelete) == null) {
+            return false;
+        } else {
+            temp = getEntry(nameToDelete);
+        }
+
+        this.mydb.remove(temp);
         return true;
     }
 
@@ -33,4 +55,17 @@ public class Datab {
         return false;
     }
 
+    /**
+     * from the unique nameAccount return the entire Entry.
+     * @param nameAccount
+     * @return Entry or null if not found
+     */
+    public ElementDb getEntry(final String nameAccount) {
+        for (int i = 0; i < mydb.size(); i++) {
+            if (this.mydb.get(i).getNameAccount() == nameAccount) {
+                return mydb.get(i);
+            }
+        }
+        return null;
+    }
 }
