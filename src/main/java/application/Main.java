ee6470b7efb23f31a6a3802e4a5bbab0a80ab3ed
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-10 17:22:30 +0200

    Add method to start javafx app

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,11 +1,23 @@
 package application;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /*
  * keepassj main
  *
  */
-public class Main {
-
-    public static void main(final String[] args) {
+public class Main extends Application{
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("KeePassJ");
+        stage.show();
+        
     }
+    
+    public static void main(final String[] args) {
+        launch();
+    }
+
 }
