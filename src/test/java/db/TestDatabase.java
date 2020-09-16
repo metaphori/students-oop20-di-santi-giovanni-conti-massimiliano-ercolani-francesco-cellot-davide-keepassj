c92c1f39563d85e82f270f40225924f47b1fe1a1
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2020-09-16 19:10:03 +0200

    Add test for Database and Entry

---
+++ src/test/java/db/TestDatabase.java
@@ -0,0 +1,50 @@
+package db;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import model.db.Database;
+import model.db.Entry;
+
+public class TestDatabase {
+
+private Database gf = null;
+
+    @org.junit.Before
+    public void initFactory() {
+        this.gf = new Database();
+    }
+
+    @org.junit.Test
+    public void testAddEntry() {
+        final Entry g = new Entry("prova", "name", "mypass00", "category", "www.prova.it", "");
+        assertNotNull(gf);
+        assertTrue(gf.addEntry(g));
+        assertFalse(gf.isEmpty());
+    }
+
+    @org.junit.Test
+    public void testDelEntry() {
+        final Entry g = new Entry("prova", "name", "mypass00", "category", "www.prova.it", "");
+        assertNotNull(gf);
+        assertTrue(gf.addEntry(g));
+        assertTrue(gf.nameAlreadyExist("prova"));
+        assertTrue(gf.delEntry("prova"));
+        assertFalse(gf.nameAlreadyExist("prova"));
+    }
+
+    @org.junit.Test
+    public void testCategory() {
+        String category = "Other";
+        //true for correct add to list
+        assertTrue(gf.addCategory(category));
+        //false for a category already added
+        assertFalse(gf.addCategory(category));
+        //true for correct remove from list
+        assertTrue(gf.delCategory(category));
+        //false for nothing to remove
+        assertFalse(gf.delCategory(category));
+    }
+}
---
+++ src/test/java/db/TestEntry.java
@@ -0,0 +1,42 @@
+package db;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import model.db.Entry;
+
+public class TestEntry {
+
+private Entry gf = null;
+
+    @org.junit.Test
+    public void testConstructor() {
+        gf = new Entry("prova", "name", "mypass00", "category", "www.prova.it", "");
+        assertNotNull(gf);
+    }
+
+    @org.junit.Test
+    public void testGetterSetter() {
+        String nameAccount = "prova";
+        String username = "username";
+        String password = "1234abcde";
+        String category = "other";
+        String url = "www.try.it";
+        String note = "no note";
+        gf = new Entry();
+        gf.setNameAccount(nameAccount);
+        gf.setUsername(username);
+        gf.setPassword(password);
+        gf.setCategory(category);
+        gf.setUrl(url);
+        gf.setNote(note);
+
+        assertNotNull(gf);
+        assertEquals(nameAccount, gf.getNameAccount());
+        assertEquals(username, gf.getUsername());
+        assertEquals(password, gf.getPassword());
+        assertEquals(category, gf.getCategory());
+        assertEquals(url, gf.getUrl());
+        assertEquals(note, gf.getNote());
+    }
+}
