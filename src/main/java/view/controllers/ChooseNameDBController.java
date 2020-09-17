dd02e83fe4b1ca921e9a278b7d569b01fdd73089
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-17 15:25:05 +0200

    Rename classes

---
+++ src/main/java/view/controllers/ChooseNameDBController.java
@@ -0,0 +1,22 @@
+package view.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ChooseNameDBController {
+
+    @FXML
+    private TextField dbName;
+
+    @FXML
+    void cancelCreation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void continueCreation(ActionEvent event) {
+
+    }
+
+}
