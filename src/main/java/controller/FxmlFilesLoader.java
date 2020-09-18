9efede4fb85f8e3bd01ea5b33da9f9a4d9f42080
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-18 18:10:29 +0200

    Add method getSceneData for fxml data passage

--- src/main/java/controller/FxmlFilesLoader.java
+++ src/main/java/controller/FxmlFilesLoader.java
@@ -6,4 +6,11 @@ public interface FxmlFilesLoader {
      * Get fxml file resource and load the stage
      */
     void getScene();
+    
+    /**
+     * Get fxml file resource, pass the data and load the stage
+     * @param data is the data to be passed
+     * @param controllerclass is the class of the controller
+     */
+    void getSceneData(DBDataSaver data, Class<?>controllerClass);
 }
--- src/main/java/controller/FxmlFilesLoaderImpl.java
+++ src/main/java/controller/FxmlFilesLoaderImpl.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import view.controllers.ChooseEncrSetController;
+import view.controllers.ChooseNameDBController;
+import view.controllers.ChoosePassController;
 
 public class FxmlFilesLoaderImpl implements FxmlFilesLoader{
     
@@ -17,6 +20,28 @@ public class FxmlFilesLoaderImpl implements FxmlFilesLoader{
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(this.source));
             Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));  
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void getSceneData(DBDataSaver data, Class<?>controllerClass) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(this.source));
+            Parent root1 = (Parent) fxmlLoader.load();
+            
+            if(controllerClass == ChooseEncrSetController.class) {
+                ChooseEncrSetController controllerOra = fxmlLoader.<ChooseEncrSetController>getController();
+                controllerOra.takeData(data);
+            } else if(controllerClass == ChoosePassController.class) {
+                ChoosePassController controllerOra = fxmlLoader.<ChoosePassController>getController();
+                controllerOra.takeData(data);
+            }
+            
             Stage stage = new Stage();
             stage.setScene(new Scene(root1));  
             stage.show();
