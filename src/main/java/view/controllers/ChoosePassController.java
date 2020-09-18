3badcfeffd2dba364bff419ea3a9429d11a6da8e
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-18 18:17:26 +0200

    Add method takeData

--- src/main/java/view/controllers/ChoosePassController.java
+++ src/main/java/view/controllers/ChoosePassController.java
@@ -1,5 +1,11 @@
 package view.controllers;
 
-public class ChoosePassController {
+import controller.DBDataSaver;
 
+public class ChoosePassController {
+    DBDataSaver data;
+    
+    public void takeData(DBDataSaver data) {
+        this.data = data;
+    }
 }
