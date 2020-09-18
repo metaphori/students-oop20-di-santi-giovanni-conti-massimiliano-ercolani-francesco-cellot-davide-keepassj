60b9ed512991e1144629fa1d62ea1a05b435f0e8
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-18 14:35:52 +0200

    Create FxmlSetter interface and class and add method setSpinner

---
+++ src/main/java/controller/FxmlSetter.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.scene.control.Spinner;
+
+public interface FxmlSetter {
+
+    /**
+     * 
+     * Set javafx component spinner for integer
+     * @param spinner is the spinner to be set
+     * @param min is the minimum limit
+     * @param max is the maximum limit
+     */
+    void setSpinner(Spinner<Integer> spinner, Integer min, Integer max);
+}
---
+++ src/main/java/controller/FxmlSetterImpl.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+
+public class FxmlSetterImpl implements FxmlSetter {
+
+    @Override
+    public void setSpinner(final Spinner<Integer> spinner, final Integer min, final Integer max) {
+        SpinnerValueFactory<Integer> values = new SpinnerValueFactory.IntegerSpinnerValueFactory(min, max);
+        spinner.setValueFactory(values);
+        spinner.setEditable(true);
+    }
+
+}
