137f58cfc9fbd2243c38bb9a2a5d33b4694a9d8a
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-18 18:15:37 +0200

    Implement methods selectEA, selectKDF and takeData

--- src/main/java/view/controllers/ChooseEncrSetController.java
+++ src/main/java/view/controllers/ChooseEncrSetController.java
@@ -3,10 +3,16 @@ package view.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import controller.DBDataSaver;
+import controller.FxmlFilesLoader;
+import controller.FxmlFilesLoaderImpl;
+import controller.FxmlSetter;
+import controller.FxmlSetterImpl;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Spinner;
@@ -16,6 +22,10 @@ import model.kdbx.KDBHeader;
 public class ChooseEncrSetController implements Initializable {
     
     KDBHeader header = new KDBHeader();
+    String source = "/resource/choosePassMenu.fxml";
+    FxmlFilesLoader loader = new FxmlFilesLoaderImpl(this.source);
+    FxmlSetter setter = new FxmlSetterImpl();
+    DBDataSaver data;
     
     @FXML
     private ComboBox<String> comboEA;
@@ -38,33 +48,64 @@ public class ChooseEncrSetController implements Initializable {
     @FXML
     private Spinner<Integer> pSpinner;
 
-    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<String> listEA = FXCollections.observableArrayList(this.header.getCipherDescriptions().keySet());
+        ObservableList<String> listKDF = FXCollections.observableArrayList(this.header.getKDFDescriptions().keySet());
+        
+        this.algDescription.setWrapText(true);
+        this.algDescription.setEditable(false);
+        this.kdfDescription.setWrapText(true);
+        this.kdfDescription.setEditable(false);
 
+        this.comboEA.setItems(listEA);
+        this.comboKDF.setItems(listKDF);        
+    }
+    
+    public void takeData(DBDataSaver data) {
+        this.data = data;
+    }
+    
     @FXML
     void selectEA(ActionEvent event) {
         String selection = comboEA.getSelectionModel().getSelectedItem().toString();
         algDescription.setText(header.getCipherDescriptions().get(selection));
+        this.data.takeCipher(selection);
     }
 
     @FXML
     void selectKDF(ActionEvent event) {
         String selection = comboKDF.getSelectionModel().getSelectedItem().toString();
         kdfDescription.setText(header.getKDFDescriptions().get(selection));
+        this.data.takeKdf(selection);
+        
+        this.setter.setSpinner(muSpinner, 1, header.getKDFMaxMemory(selection));
+        this.setter.setSpinner(pSpinner, 1, header.getKDFMaxMemory(selection));
+        this.setter.setSpinner(trSpinner, 1, header.getKDFRounds(selection));
+        
+        if(header.isKDFTweakable(selection) == false) {
+            this.muSpinner.setDisable(true);
+            this.pSpinner.setDisable(true);
+        }
+    }
+    
+    @FXML
+    void cancelCreation(ActionEvent event) {
+
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        ObservableList<String> listEA = FXCollections.observableArrayList(header.getCipherDescriptions().keySet());
-        ObservableList<String> listKDF = FXCollections.observableArrayList(header.getKDFDescriptions().keySet());
+    @FXML
+    void continueCreation(ActionEvent event) {
+        this.data.takeRounds(this.trSpinner.getValue());
+        this.data.takeMemory(this.muSpinner.getValue());
+        this.data.takeParallelism(this.pSpinner.getValue());
         
-        algDescription.setWrapText(true);
-        algDescription.setEditable(false);
-        kdfDescription.setWrapText(true);
-        kdfDescription.setEditable(false);
-
-        comboEA.setItems(listEA);
-        comboKDF.setItems(listKDF);
-        
+        this.loader.getSceneData(this.data, ChoosePassController.class);
     }
 
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+    
 }
