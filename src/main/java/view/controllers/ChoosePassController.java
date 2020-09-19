3079ed6cebd0aa8d239627dc29773fbaa01d962a
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-19 09:48:04 +0200

    Add method for each fxml element and implement confirmCreation

--- src/main/java/view/controllers/ChoosePassController.java
+++ src/main/java/view/controllers/ChoosePassController.java
@@ -1,11 +1,53 @@
 package view.controllers;
 
+import java.awt.event.ActionEvent;
+
 import controller.DBDataSaver;
+import controller.FxmlSetter;
+import controller.FxmlSetterImpl;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import model.kdbx.KDBHeader;
 
 public class ChoosePassController {
     DBDataSaver data;
+    private FxmlSetter setter = new FxmlSetterImpl();
+    KDBHeader header = new KDBHeader();
+  
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private PasswordField passwordRepeat;
     
     public void takeData(DBDataSaver data) {
         this.data = data;
+    }
+    @FXML
+    void cancelCreation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmCreation(ActionEvent event) {
+        if(passwordRepeat == password) {
+            header.setComment(data.getDBName());
+            header.setPublicCustomData(data.getDBDesc());
+            header.setCipher(data.getCipher());
+            header.setKDF(data.getKdf());
+            //setrounds
+            if(data.getTweakable() == true) {
+                header.setKDFMemory(data.getMemory());
+                header.setKDFParallelism(data.getParallelism());
+            }
+            
+        } else {
+            setter.warningDialog("Passwords are not the same");
+        }
+    }
+
+    @FXML
+    void goBack(ActionEvent event) {
+
     }
 }
