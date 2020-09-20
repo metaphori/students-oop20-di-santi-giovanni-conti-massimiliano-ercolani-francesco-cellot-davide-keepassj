82cc3dfb94b9e9bfd919ad3bba547fc2614ddbb0
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-20 14:38:21 +0200

    Modify GUItest

--- src/main/java/view/GUITest.java
+++ src/main/java/view/GUITest.java
@@ -1,19 +1,27 @@
 package view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 
 public class GUITest extends Application {
     public static void main(final String[] args) {
-        launch(args);
+        Application.launch();
     }
 
-    public final void start(final Stage primaryStage) {
-        Label label = new Label("Hello World");
-        Scene scene = new Scene(label);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public final void start(final Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/buba.fxml"));
+            Parent root1 = fxmlLoader.load();
+            
+            stage.setTitle("KeePassJ");
+            stage.setScene(new Scene(root1));  
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
