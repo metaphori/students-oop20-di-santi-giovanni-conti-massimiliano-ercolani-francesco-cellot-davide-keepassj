6e0206fbc2c55ca41da8a5caa226c51d409440d1
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-20 08:27:38 +0200

    Implement method confirmCreation in choopasscontroller class

--- src/main/java/view/controllers/ChoosePassController.java
+++ src/main/java/view/controllers/ChoosePassController.java
@@ -1,19 +1,29 @@
 package view.controllers;
 
-import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import controller.DBDataSaver;
 import controller.FxmlSetter;
 import controller.FxmlSetterImpl;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+import model.kdbx.KDB;
 import model.kdbx.KDBHeader;
 
 public class ChoosePassController {
-    DBDataSaver data;
+    private DBDataSaver data;
     private FxmlSetter setter = new FxmlSetterImpl();
-    KDBHeader header = new KDBHeader();
-  
+    private KDBHeader header = new KDBHeader();
+    private KDB database;
+    private FileChooser fileChooser;
+    private List<byte[]> creadentials;
     @FXML
     private PasswordField password;
 
@@ -35,10 +45,23 @@ public class ChoosePassController {
             header.setPublicCustomData(data.getDBDesc());
             header.setCipher(data.getCipher());
             header.setKDF(data.getKdf());
-            //setrounds
+            header.setTransformRounds(data.getRounds());
             if(data.getTweakable() == true) {
                 header.setKDFMemory(data.getMemory());
                 header.setKDFParallelism(data.getParallelism());
+            }
+            fileChooser = new FileChooser();
+            Stage stage = setter.getStage(event);
+            fileChooser.setTitle("Save database as");
+            fileChooser.getExtensionFilters().addAll(new ExtensionFilter("All Files", "*.*"));
+            File file = fileChooser.showSaveDialog(stage);
+            creadentials = Arrays.asList(password.toString().getBytes());
+            
+            try {
+                database = new KDB(file, creadentials, header);
+                database.write(new byte[0]);
+            } catch(IOException e) {
+                e.printStackTrace();
             }
             
         } else {
