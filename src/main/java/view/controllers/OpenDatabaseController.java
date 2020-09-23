c1b4f372da462526d41ee7b35150210ad4884d00
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-23 18:46:16 +0200

    Add method takeFile to openDatabaseController

--- src/main/java/view/controllers/OpenDatabaseController.java
+++ src/main/java/view/controllers/OpenDatabaseController.java
@@ -1,5 +1,26 @@
 package view.controllers;
 
-public class OpenDatabaseController {
+import java.io.File;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+public class OpenDatabaseController {
+    @FXML
+    private PasswordField password;
+    File file;
+
+    public void takeFile(File file) {
+        this.file = file;
+    }
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openDatabase(ActionEvent event) {
+
+    }
 }
