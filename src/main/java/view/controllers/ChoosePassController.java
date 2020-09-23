5d50ae2a40be0e60c9109f4995e38db144cdf7a4
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-23 17:16:43 +0200

    Correct cancel method problem in choosePassController

--- src/main/java/view/controllers/ChoosePassController.java
+++ src/main/java/view/controllers/ChoosePassController.java
@@ -45,6 +45,7 @@ public class ChoosePassController {
     }
     @FXML
     void cancelCreation(ActionEvent event) {
+        loader.getMainMenuScene();
         setter.getStage(event).close();
     }
 
