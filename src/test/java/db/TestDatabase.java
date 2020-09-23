53b3548594a11926e1242647d02d94a899b0ef6f
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2020-09-23 10:33:13 +0200

    Add test for get multiple Entrys

--- src/test/java/db/TestDatabase.java
+++ src/test/java/db/TestDatabase.java
@@ -1,8 +1,12 @@
 package db;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import model.db.Database;
 import model.db.Entry;
@@ -10,29 +14,29 @@ import model.db.Group;
 
 public class TestDatabase {
 
-private Database gf = null;
+private Database myDb = null;
 
     @org.junit.Before
     public void initFactory() {
-        this.gf = new Database();
+        this.myDb = new Database();
     }
 
     @org.junit.Test
     public void testAddEntry() {
         final Entry g = new Entry();
-        assertNotNull(gf);
-        assertTrue(gf.addEntry(g));
-        assertFalse(gf.isEmpty());
+        assertNotNull(myDb);
+        assertTrue(myDb.addEntry(g));
+        assertFalse(myDb.isEmpty());
     }
 
     @org.junit.Test
     public void testDelEntry() {
         final Entry g = new Entry();
-        assertNotNull(gf);
-        assertTrue(gf.addEntry(g));
-        assertTrue(gf.nameAlreadyExist("prova"));
-        assertTrue(gf.deleteEntry(g));
-        assertFalse(gf.nameAlreadyExist("prova"));
+        assertNotNull(myDb);
+        assertTrue(myDb.addEntry(g));
+        assertTrue(myDb.nameAlreadyExist("prova"));
+        assertTrue(myDb.deleteEntry(g));
+        assertFalse(myDb.nameAlreadyExist("prova"));
     }
 
     @org.junit.Test
@@ -40,20 +44,38 @@ private Database gf = null;
         Group group = new Group("Other");
         Entry app = new Entry();
         app.setGroup(group);
-        gf.addEntry(app);
+        myDb.addEntry(app);
 
         //true for correct add to list
-        assertTrue(gf.addGroup(group));
+        assertTrue(myDb.addGroup(group));
         //false for a group already added
-        assertFalse(gf.addGroup(group));
+        assertFalse(myDb.addGroup(group));
         //can't remove from list for 1 item in that group
-        assertFalse(gf.delGroup(group));
+        assertFalse(myDb.delGroup(group));
 
         //remove item used for test
-        gf.deleteEntry(app);
+        myDb.deleteEntry(app);
         //true for correct remove
-        assertTrue(gf.delGroup(group));
+        assertTrue(myDb.delGroup(group));
         //false for nothing to remove
-        assertFalse(gf.delGroup(group));
+        assertFalse(myDb.delGroup(group));
+    }
+
+    @org.junit.Test
+    public void testGetterEntrys() {
+        Entry one = new Entry();
+        one.setNameAccount("one");
+        Entry two = new Entry();
+        two.setNameAccount("two");
+
+        myDb.addEntry(one);
+        myDb.addEntry(two);
+
+        ArrayList<Entry> list = new ArrayList<>();
+        list.add(one);
+        list.add(two);
+
+        assertEquals(list, myDb.getAllEntry());
+        assertEquals(list, myDb.getAllEntryOfSpecifiedGroup(new Group("prova")));
     }
 }
