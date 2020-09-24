378c1ae6ba74c69dc8e5255c0afd778205060152
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-24 09:37:48 +0200

    Add methods to addentry and managemenu controllers

--- src/main/java/view/controllers/AddEntryController.java
+++ src/main/java/view/controllers/AddEntryController.java
@@ -1,5 +1,32 @@
 package view.controllers;
 
-public class AddEntryController {
+import java.awt.TextField;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+public class AddEntryController {
+    
+    @FXML
+    private TextField title;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField url;
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmAdd(ActionEvent event) {
+
+    }
 }
--- src/main/java/view/controllers/ManageMenuController.java
+++ src/main/java/view/controllers/ManageMenuController.java
@@ -1,5 +1,12 @@
 package view.controllers;
 
-public class ManageMenuController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
+public class ManageMenuController {
+    
+    @FXML
+    void addEntry(ActionEvent event) {
+
+    }
 }
