c0e07da81e3cd4e2be65df39883562777e665253
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2020-09-25 12:27:43 +0200

    Add hashcode to Entry e Group class

--- src/main/java/model/db/Entry.java
+++ src/main/java/model/db/Entry.java
@@ -121,4 +121,59 @@ public class Entry {
     public void setNote(final String note) {
         this.note = note;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((groupName == null) ? 0 : groupName.hashCode());
+        result = prime * result + ((nameAccount == null) ? 0 : nameAccount.hashCode());
+        result = prime * result + ((note == null) ? 0 : note.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        result = prime * result + ((url == null) ? 0 : url.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Entry other = (Entry) obj;
+        if (groupName == null) {
+            if (other.groupName != null)
+                return false;
+        } else if (!groupName.equals(other.groupName))
+            return false;
+        if (nameAccount == null) {
+            if (other.nameAccount != null)
+                return false;
+        } else if (!nameAccount.equals(other.nameAccount))
+            return false;
+        if (note == null) {
+            if (other.note != null)
+                return false;
+        } else if (!note.equals(other.note))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        if (url == null) {
+            if (other.url != null)
+                return false;
+        } else if (!url.equals(other.url))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
 }
