91c0e64b9e439ca01283fdd0fa1e61a7b6f56d9f
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2020-09-27 15:25:02 +0200

    Add button to deleteEntry and tableView for group list

--- src/main/java/view/controllers/ManageMenuController.java
+++ src/main/java/view/controllers/ManageMenuController.java
@@ -1,6 +1,7 @@
 package view.controllers;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javax.xml.bind.JAXBException;
@@ -18,9 +19,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-
+import javafx.scene.text.Text;
 import model.db.Database;
 import model.db.Entry;
+import model.db.Group;
 
 /**
  * 
@@ -38,8 +40,12 @@ public class ManageMenuController implements Initializable {
     private TableView<Entry> accountTable;
 
     @FXML
+    private TableView<Group> groupTable;
+
+    @FXML
     private Label labelNomeDatabase;
 
+    @SuppressWarnings("unchecked")
     @Override
     public final void initialize(final URL location, final ResourceBundle resources) {
         TableColumn<Entry, String> title = new TableColumn<Entry, String>("Title");
@@ -49,13 +55,44 @@ public class ManageMenuController implements Initializable {
         TableColumn<Entry, String> url = new TableColumn<Entry, String>("Url");
         TableColumn<Entry, String> note = new TableColumn<Entry, String>("Note");
 
-        accountTable.getColumns().addAll(title, username, password, group, url, note);
+        this.accountTable.getColumns().addAll(title, username, password, group, url, note);
 
         title.setCellValueFactory(new PropertyValueFactory<Entry, String>("nameAccount"));
         username.setCellValueFactory(new PropertyValueFactory<Entry, String>("username"));
+        password.setCellValueFactory(new PropertyValueFactory<Entry, String>("password"));
         group.setCellValueFactory(new PropertyValueFactory<Entry, String>("groupName"));
         url.setCellValueFactory(new PropertyValueFactory<Entry, String>("url"));
         note.setCellValueFactory(new PropertyValueFactory<Entry, String>("note"));
+
+        TableColumn<Group, String> nameGroup = new TableColumn<Group, String>("Name");
+        TableColumn<Group, String> descGroup = new TableColumn<Group, String>("Description");
+        this.groupTable.getColumns().addAll(nameGroup, descGroup);
+        nameGroup.setCellValueFactory(new PropertyValueFactory<Group, String>("name"));
+        descGroup.setCellValueFactory(new PropertyValueFactory<Group, String>("description"));
+    }
+
+    public final void autoResizeColumns() {
+        //Set the right policy
+        this.accountTable.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
+        this.accountTable.getColumns().stream().forEach((column) -> {
+            //Minimal width = columnheader
+            Text t = new Text(column.getText());
+            double max = t.getLayoutBounds().getWidth();
+            for (int i = 0; i < this.accountTable.getItems().size(); i++) {
+                //cell must not be empty
+                if (column.getCellData(i) != null) {
+                    t = new Text(column.getCellData(i).toString());
+                    double calcwidth = t.getLayoutBounds().getWidth();
+                    //remember new max-width
+                    if (calcwidth > max) {
+                        max = calcwidth;
+                    }
+                }
+            }
+            //set the new max-widht with some extra space
+            column.setPrefWidth(max + 10.0d);
+        }
+        );
     }
 
     /**
@@ -67,9 +104,16 @@ public class ManageMenuController implements Initializable {
         this.db = db;
         //List<String> app = db.getAllEntry().stream().map(Entry::getNameAccount).collect(Collectors.toList());
         //List<String> app = db.getAllEntry().stream().map(Entry::getNameAccount).collect(Collectors.toCollection(ArrayList::new));
-        ObservableList<Entry> listTemp = FXCollections.observableArrayList(db.getAllEntry());
-        accountTable.setItems(listTemp);
+        updateTableView();
         this.labelNomeDatabase.setText(this.db.getNomeDatabase());
+    }
+
+    private void updateTableView() {
+        ObservableList<Entry> entryTemp = FXCollections.observableArrayList(db.getAllEntry());
+        accountTable.setItems(entryTemp);
+        autoResizeColumns();
+        ObservableList<Group> groupTemp = FXCollections.observableArrayList(db.getAllGroup());
+        groupTable.setItems(groupTemp);
     }
 
     @FXML
@@ -82,6 +126,18 @@ public class ManageMenuController implements Initializable {
     final void addGroup(final ActionEvent event) {
         groupLoader.getSceneGroup(db);
         setter.getStage(event).close();
+    }
+
+    @FXML
+    final void deleteSelectedEntry(final ActionEvent event) {
+        ArrayList<Entry> p = new ArrayList<>(this.accountTable.getSelectionModel().getSelectedItems());
+        /*for (Entry res : p) {
+            this.db.deleteEntry(res);
+          }*/
+        p.stream().forEach(e -> this.db.deleteEntry(e));
+        //Entry entryTemp = this.accountTable.getSelectionModel().getSelectedItem();
+        //this.db.deleteEntry(entryTemp);
+        updateTableView();
     }
 
 }
