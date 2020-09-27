00503562b59d447bd7f756ac73f5285ff624b367
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2020-09-27 14:47:54 +0200

    Fix TableView to show entryList correctly

--- src/main/java/view/controllers/ManageMenuController.java
+++ src/main/java/view/controllers/ManageMenuController.java
@@ -1,16 +1,23 @@
 package view.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javax.xml.bind.JAXBException;
 
 import controller.FxmlFilesLoader;
 import controller.FxmlFilesLoaderImpl;
 import controller.FxmlSetter;
 import controller.FxmlSetterImpl;
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import model.db.Database;
 import model.db.Entry;
@@ -20,7 +27,7 @@ import model.db.Entry;
  * Controller for ManageMenu.fxml file.
  *
  */
-public class ManageMenuController {
+public class ManageMenuController implements Initializable {
     private final FxmlFilesLoader entryLoader = new FxmlFilesLoaderImpl();
     private final FxmlFilesLoader groupLoader = new FxmlFilesLoaderImpl();
     private final FxmlSetter setter = new FxmlSetterImpl();
@@ -33,6 +40,23 @@ public class ManageMenuController {
     @FXML
     private Label labelNomeDatabase;
 
+    @Override
+    public final void initialize(final URL location, final ResourceBundle resources) {
+        TableColumn<Entry, String> title = new TableColumn<Entry, String>("Title");
+        TableColumn<Entry, String> username = new TableColumn<Entry, String>("Username");
+        TableColumn<Entry, String> password = new TableColumn<Entry, String>("Password");
+        TableColumn<Entry, String> group = new TableColumn<Entry, String>("Group"); 
+        TableColumn<Entry, String> url = new TableColumn<Entry, String>("Url");
+        TableColumn<Entry, String> note = new TableColumn<Entry, String>("Note");
+
+        accountTable.getColumns().addAll(title, username, password, group, url, note);
+
+        title.setCellValueFactory(new PropertyValueFactory<Entry, String>("nameAccount"));
+        username.setCellValueFactory(new PropertyValueFactory<Entry, String>("username"));
+        group.setCellValueFactory(new PropertyValueFactory<Entry, String>("groupName"));
+        url.setCellValueFactory(new PropertyValueFactory<Entry, String>("url"));
+        note.setCellValueFactory(new PropertyValueFactory<Entry, String>("note"));
+    }
 
     /**
      * Takes database from previous fxml file.
@@ -43,8 +67,8 @@ public class ManageMenuController {
         this.db = db;
         //List<String> app = db.getAllEntry().stream().map(Entry::getNameAccount).collect(Collectors.toList());
         //List<String> app = db.getAllEntry().stream().map(Entry::getNameAccount).collect(Collectors.toCollection(ArrayList::new));
-        //ObservableList<Entry> app = FXCollections.<Entry>observableArrayList();
-        accountTable.getItems().setAll(db.getAllEntry());
+        ObservableList<Entry> listTemp = FXCollections.observableArrayList(db.getAllEntry());
+        accountTable.setItems(listTemp);
         this.labelNomeDatabase.setText(this.db.getNomeDatabase());
     }
 
