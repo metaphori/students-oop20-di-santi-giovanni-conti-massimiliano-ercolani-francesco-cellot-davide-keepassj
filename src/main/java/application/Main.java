56b9be2750b3af4a91549b3095039f9033e33c75
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-09-28 12:21:46 +0200

    Modify start of the application

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,28 +1,7 @@
 package application;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import view.View;
-import view.ViewImpl;
-
-/**
- * 
- * Main for application start.
- *
- */
-public class Main extends Application {
-
-    @Override
-    public final void start(final Stage stage) throws Exception {
-        View view = new ViewImpl();
-        view.loadFirstScene(stage);
-    }
-
-    /**
-     * KeePassJ launch.
-     * @param args 
-     */
-    public static void main(final String[] args) {
-        Application.launch();
+public class Main {
+    public static void main(String[] args) {
+        StartFX.main(args);
     }
 }
